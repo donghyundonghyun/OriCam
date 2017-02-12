8570b7115a9b35f07dcf35531a890922bf5778b9
jeonghyun <38jh1116@naver.com>
2017-02-13 01:24:37 +0900

    건물 정보 페이지 하단 textview를 listview 형식으로 수정. 지금은 광개토관 정보만 넣어둔 상황. 서버에서 받아오게끔 수정해야함.

--- app/src/main/java/com/example/donghyun/myhack/InfoActivity.java
+++ app/src/main/java/com/example/donghyun/myhack/InfoActivity.java
@@ -4,13 +4,16 @@ package com.example.donghyun.myhack;
  * Created by DongHyun on 2016-12-21.
  */
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+// 건물 정보를 보여줌
+// 건물 이름,이미지,전화번호,편의시설,기타 정보를 현재는 그냥 넣어놓은 상황 -> 서버에서 받아와야함
 public class InfoActivity extends AppCompatActivity {
 
     @Override
@@ -19,19 +22,15 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        Intent intent = getIntent();
-        int index = intent.getIntExtra("index",1);
+        //Intent intent = getIntent();
+        //int index = intent.getIntExtra("index",1);
 
         // 건물 이미지
-        int gwangImg = R.drawable.gwang1;
-        int studenthallImg = R.drawable.studenthall1;
-
+        int building_Img = R.drawable.gwang1;
         // 건물 이름
-        String gwangName = "광개토관";
-        String studentHallName="학생회관";
-
+        String building_Name = "광개토관";
         // 건물 정보
-        String gwangContents = "경영학과가 주로 사용하는 건물이며 강의실 뿐 아니라 다양한 편의시설을 갖추고 있다.\n" +
+        String building_Contents = "경영학과가 주로 사용하는 건물이며 강의실 뿐 아니라 다양한 편의시설을 갖추고 있다.\n" +
                 "5층에는 경영학과 학생들이 운영하는 카페가 있다.\n\n"+
                 "지하 3,4층 : 주차장\n"+
                 "지하 2층 : 컨벤션 센터\n"+
@@ -39,33 +38,29 @@ public class InfoActivity extends AppCompatActivity {
                 "지상 1층 ~ 13층 : 강의실 및 연구실\n"+
                 "지상 14층 : 외국인 학생 기숙사\n"+
                 "지상 15층 : 식당(찬), 소극장";
-        String studentHallContents = "학생들을 위한 건물로 동아리실과 다양한 편의시설을 갖추고 있다.\n\n" +
-                "지하 2층 : 체력단련실, 샤워실, 음악 연습실, 동아리실\n" +
-                "지하 1층 : 대공연장, 소공연장, 학생식당 \n" +
-                "1층 : 편의점, 라운지, 푸드코트\n" +
-                "2층 : 의무실, 카페베네\n" +
-                "3층 : 챌린지 대원 전용 공간, 취업진로 지원 센터, JOB카페, 학생생활상담소, 클라이밍실\n" +
-                "4층 : 총 학생회실 단과대학생 회실, 동아리 연합회실, 동아리실\n" +
-                "5층 : 동아리실\n" +
-                "6층 : 동아리실, 방송국, 세종타임즈, 학보사\n";
+        //건물 전화번호
+        String building_Phone = "02) 3408-1234";
+        //건물 편의시설
+        String building_Facility = "식당, 카페, 매점, 화장실";
 
         // activity_info의 건물 이미지,이름,정보 내용을 설정
         ImageView img = (ImageView)findViewById(R.id.buildingImg);
         TextView name = (TextView)findViewById(R.id.buildingName);
-        TextView info = (TextView)findViewById(R.id.buildingInfo);
-        info.setMovementMethod(new ScrollingMovementMethod());
 
-        if(index == 2) {
-            img.setBackgroundResource(studenthallImg);
-            name.setText(studentHallName);
-            info.setText(studentHallContents);
-        }
-        else if(index == 1){
-            img.setBackgroundResource(gwangImg);
-            name.setText(gwangName);
-            info.setText(gwangContents);
-        }
+        img.setBackgroundResource(building_Img);
+        name.setText(building_Name);
 
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        adapter = new ListViewAdapter() ;
+        listview = (ListView) findViewById(R.id.buildingInfo);
+        listview.setAdapter(adapter);
+        listview.setBackgroundColor(Color.WHITE);
+
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.phone),building_Phone) ;
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.facility),building_Facility);
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.info),building_Contents);
 
     }
 }
